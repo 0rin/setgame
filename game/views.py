6a4ce961d98edd661bdd8a460300ec6abbfe07ad
0rin <orinhabich@gmail.com>
2020-05-02 19:39:24 +0200

    Initial commit

---
+++ game/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+import random
+
+def home(request):
+    deck = [{'color': color,
+             'shading': shading,
+             'range': range(number),
+             'number': number,
+             'shape': shape} for color in ['red', 'green', 'blue']
+                             for number in [1, 2, 3]
+                             for shading in ['solid', 'striped', 'open']
+                             for shape in ['oval', 'diamond', 'rectangle']]
+    random.shuffle(deck)
+    setup_cards = [deck.pop() for i in range(12)]
+    # return HttpResponse(f"{setup_cards} {len(deck)}")
+    return render(request, 'game/home.html', {'setup_cards': setup_cards})
---
+++ setgame/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for setgame project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setgame.settings')
+
+application = get_asgi_application()
