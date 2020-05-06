5256f1a946c460040d32333f85f7d6ef6286495f
0rin <orinhabich@gmail.com>
2020-05-06 09:48:31 +0200

    Add validation of set

--- game/static/game/js/home.js
+++ game/static/game/js/home.js
@@ -1,25 +1,60 @@
 var selected_cards = [];
-function myFunction(card_id) {
-    console.log(card_id)
-    // console.log(document.getElementById(card_id))
+var colors = []
+var numbers = []
+var shadings = []
+var shapes = []
+
+function cardClicked(card_id, color, number, shading, shape) {
     if (selected_cards.includes(card_id)) {
         var card_index = selected_cards.indexOf(card_id);
         document.getElementById(card_id).style.background = 'white';
-        console.log(card_index)
-        selected_cards.splice(card_index, 1);
+        removeFromArrays(card_index, color, number, shading, shape);
     }
     else {
         document.getElementById(card_id).style.background = 'grey';
-        selected_cards.push(card_id)
+        addToArrays(card_id, color, number, shading, shape);
         if (selected_cards.length == 3) {
-            console.log('Ja, 3 kaarten. Maar is het ook een set?')
-            // loop over selected_cards and for each
+            validateSet([colors, numbers, shadings, shapes]);
             selected_cards.forEach(card => {
                 document.getElementById(card).style.background = 'white';
             });
-            selected_cards = [];
+            resetArrays();
         }
     }
+}
 
-    console.log('selected_cards are', selected_cards);
+
+function validateSet(properties) {
+    for (i = 0; i < properties.length   ; i++) {
+        uniq = [...new Set(properties[i])];
+        if (uniq.length == 2) {
+            alert('Sorry, this was not a set.');
+            return
+        }
+    };
+    alert('SET!')
+}
+
+function addToArrays(card_id, color, number, shading, shape) {
+    selected_cards.push(card_id);
+    colors.push(color);
+    numbers.push(number);
+    shadings.push(shading);
+    shapes.push(shape);
+}
+
+function removeFromArrays(card_index, color, number, shading, shape) {
+    selected_cards.splice(card_index, 1);
+    colors.splice(card_index, 1);
+    numbers.splice(card_index, 1);
+    shadings.splice(card_index, 1);
+    shapes.splice(card_index, 1);
+}
+
+function resetArrays() {
+    selected_cards = [];
+    colors = [];
+    numbers = [];
+    shadings = [];
+    shapes = [];
 }
